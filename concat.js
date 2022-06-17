579c90103454ed6fed0b91ae86052da0d6bcf110
Romina Cepele <rcepele@unitsystem.net>
2022-06-17 11:31:06 +0200

    day2

--- concat.js
+++
@@ -1,5 +0,0 @@
-export function concatArrays(numbers) {
-    let arr2 = [5,6,7];
-
-   return numbers.concat(arr2);
-}
--- sum.js
+++
@@ -1,8 +0,0 @@
-export function summ (array3) {
-    let sum = 0;
-    for (let i = 0 ; i<array3.length ; i++){
-    sum = sum + array3[i];
- }   
-    return  sum;
-
-}
